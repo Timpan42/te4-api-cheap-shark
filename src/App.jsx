9eaa0ff73ca7c3bd9933a978d260f3825b840d90
Timpan42 <tim@fagerdal.se>
2023-09-28 14:09:28 +0200

    🚀

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <article>
+        <h1> When a person searches for a game the best deals shows</h1>
+        <h2> Using <a href="https://apidocs.cheapshark.com/#intro">CheapSharks API</a> </h2>
+      </article>
+    </>
+  )
+}
+
+export default App
